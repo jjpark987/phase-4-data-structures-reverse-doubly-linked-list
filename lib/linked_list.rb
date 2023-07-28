10d705e2d8cf01925f01246c993e56952ac0711f
Jason Park <jjpark987@gmail.com>
2023-07-28 13:25:10 -0700

    commit tree 0025e36d0e33a6c79e84dc9e90d10f02006a87a6

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head, :tail
@@ -9,6 +10,44 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return if !@head && !@tail || @head == @tail
+
+    new_head = @tail
+    new_tail = @head
+
+    curr_node = @head
+
+    while curr_node
+      reverse_node curr_node
+      curr_node = curr_node.prev_node
+    end
+
+    @head = new_head
+    @tail = new_tail
+
+    self
+  end
+
+  def reverse_node node
+    new_next = node.prev_node
+    new_prev = node.next_node
+
+    node.next_node = new_next
+    node.prev_node = new_prev
+
+    node
   end
 end
+
+# l = LinkedList.new
+# n1 = Node.new(1)
+# n2 = Node.new(2)
+# n3 = Node.new(3)
+# l.head = n1
+# n1.next_node = n2
+# n2.prev_node = n1
+# n2.next_node = n3
+# n3.prev_node = n2
+# l.tail = n3
+
+
